a5866abe66a81758692ab8494ad92073f20ae642
Faiz <faizannabil7002@gmail.com>
2026-07-11 20:19:45 +0700

    fix: map missing notification types to redirect links for applicants and HR

--- resources/views/layouts/pelamar.blade.php
+++ resources/views/layouts/pelamar.blade.php
@@ -395,6 +395,16 @@
                 svg: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>',
                 link: '/pelamar/status-lamaran'
             },
+            interview_rescheduled: {
+                bg: 'bg-purple-100', color: 'text-purple-600',
+                svg: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>',
+                link: '/pelamar/status-lamaran'
+            },
+            interview_cancelled: {
+                bg: 'bg-red-100', color: 'text-red-600',
+                svg: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"/>',
+                link: '/pelamar/status-lamaran'
+            },
             privilege_change: {
                 bg: 'bg-amber-100', color: 'text-amber-600',
                 svg: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"/>',
